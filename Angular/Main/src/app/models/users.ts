d9e67afef04cd3ff1c28884c448ac37d443c5853
gmkhokababu <gmakhokababu@gmail.com>
2024-04-30 03:24:34 +0600

    creae user model

--- Angular/Main/src/app/models/users.ts
+++ Angular/Main/src/app/models/users.ts
@@ -1,2 +1,16 @@
 export class Users {
+    id:any;
+    name:any;
+    email:any;
+    userName:any;
+    password:any;
+    role:any;
+    constructor(id:any,name:any,email:any,userName:any,password:any,role:any){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.userName=userName;
+        this.password=password;
+        this.role=role;
+    }
 }
