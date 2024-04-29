ff2234526db85bc5ab75f7c3ab883ced9d7e4f25
gmkhokababu <gmakhokababu@gmail.com>
2024-04-30 03:06:31 +0600

    Create login and visitor ui

--- Angular/Main/src/app/app.module.ts
+++ Angular/Main/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { HomeComponent } from './components/visitor/home/home.component';
+import { AboutComponent } from './components/visitor/about/about.component';
+import { LoginComponent } from './components/visitor/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
